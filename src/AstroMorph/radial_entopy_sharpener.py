65e0f371c357f35a66e9b89fc8ad4bb777f12667
SpaceMarsBalloon <stevetpaquette@gmail.com>
2025-06-23 23:12:37 -0400

    updates

--- src/AstroMorph/radial_entopy_sharpener.py
+++ src/AstroMorph/radial_entopy_sharpener.py
@@ -5,6 +5,8 @@ from scipy.ndimage import gaussian_filter, generic_filter
 from skimage.util import img_as_ubyte
 from skimage.filters.rank import entropy
 from skimage.morphology import disk
+from skimage.exposure import rescale_intensity
+import matplotlib.pyplot as plt
 
 
 def local_entropy(image, neighborhood=9):
@@ -21,24 +23,26 @@ def radial_weight_map(shape, center, power=2):
     return 1 - r_norm ** power
 
 
-def entropy_sharpen(image, cluster_center, entropy_scale=1.0, contrast_boost=2.0):
-    """
-    Returns a radial distance sharpened image using entropic local weighting.
+def entropy_sharpen(image, cluster_center, entropy_scale=1.0, contrast_boost=2.0, power=2.0):
+    if image.ndim == 3:
+        from skimage.color import rgb2gray
+        image = rgb2gray(image)
 
-    :param image:
-    :param cluster_center:
-    :param entropy_scale:
-    :param contrast_boost:
-    :return:
-    """
     image = img_as_float(image)
     ent = local_entropy(image)
-    weights = radial_weight_map(image.shape, cluster_center)
+    weights = radial_weight_map(image.shape, cluster_center, power=power)  # 👈 updated here
 
-    # Normalize entropy and mix it into the image
-    ent_norm = (ent - ent.min()) / (ent.ptp())
+    # ent_norm = (ent - ent.min()) / (ent.ptp())
+    ent_norm = (ent - ent.min()) / np.ptp(ent)
     ent_weighted = ent_norm * weights
     enhanced = image + entropy_scale * (ent_weighted * (image - gaussian_filter(image, sigma=2)))
+    # enhanced = rescale_intensity(enhanced, in_range='image', out_range=(0, 1))
 
-    # Contrast enhancement
+    p_low, p_high = np.percentile(enhanced, (0.5, 99.5))
+    enhanced = rescale_intensity(enhanced, in_range=(p_low, p_high), out_range=(0, 1))
+
+    print("Before rescale:", enhanced.min(), enhanced.max())
+    print("Rescaling range:", p_low, "to", p_high)
+
     return exposure.adjust_gamma(enhanced, gamma=1.0 / contrast_boost)
+
--- src/AstroMorph/ui_contrast_slider.py
+++ src/AstroMorph/ui_contrast_slider.py
@@ -3,10 +3,15 @@ from matplotlib.widgets import Slider
 from skimage import io
 from radial_entopy_sharpener import entropy_sharpen
 
+
 def launch_entropy_sharpen_UI(image, cluster_center):
     fig, ax = plt.subplots()
     plt.subplots_adjust(left=0.25, bottom=0.25)
-    img_disp = ax.imshow(image, cmap='gray')
+    # img_disp = ax.imshow(image, cmap='gray')
+    img_disp = ax.imshow(image, cmap='inferno')
+    # diff_img = np.clip(new_img - image, 0, 1)
+    # ax_diff.imshow(diff_img, cmap='inferno')
+    # ax_diff.set_title("Sharpening Difference Map")
     ax.set_title("Entropy Sharpen Preview")
 
     # Slider axes
@@ -14,21 +19,35 @@ def launch_entropy_sharpen_UI(image, cluster_center):
     ax_contrast = plt.axes([0.25, 0.10, 0.65, 0.03])
     ax_power = plt.axes([0.25, 0.05, 0.65, 0.03])
 
-    s_entropy = Slider(ax_entropy, 'Entropy Scale', 0.0, 2.0, valinit=1.0)
-    s_contrast = Slider(ax_contrast, 'Contrast Boost', 0.5, 3.0, valinit=1.5)
-    s_power = Slider(ax_power, 'Radial Power', 0.5, 5.0, valinit=2.0)
+    s_entropy = Slider(ax_entropy, 'Entropy Scale', 0.01, 0.70, valinit=0.10)
+    s_contrast = Slider(ax_contrast, 'Contrast Boost', 0.05, 0.50, valinit=0.15)
+    s_power = Slider(ax_power, 'Radial Power', 0.05, 100.00, valinit=0.20)
+
+    # weights = radial_weight_map(image.shape, cluster_center, power=s_power.val)
+    # ax.contour(weights, levels=5, colors='cyan', alpha=0.3)
 
     def update(val):
         new_img = entropy_sharpen(
             image,
             cluster_center,
             entropy_scale=s_entropy.val,
-            contrast_boost=s_contrast.val
+            contrast_boost=s_contrast.val,
+            power=s_power.val  # 👈 now interactive!
         )
+        print("New image range:", new_img.min(), new_img.max())
         img_disp.set_data(new_img)
-        fig.canvas.draw_idle()
+        img_disp.set_clim(0, 1)
+        fig.canvas.draw()
+
 
     for slider in [s_entropy, s_contrast, s_power]:
         slider.on_changed(update)
 
     plt.show()
+
+
+
+
+if __name__=="__main__":
+    image = io.imread("C:/Users/Futurama/Documents/AstroMorph/AstroMorph/data/M92_cropped.jpg")
+    launch_entropy_sharpen_UI(image,(image.shape[0] // 2, image.shape[1] // 2))
